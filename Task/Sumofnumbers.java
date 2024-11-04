35798c38f0efc6000f806dd0cebb0e237a054dfe
vathana <136918722+Vathanavaradharaj@users.noreply.github.com>
2024-11-04 10:37:45 +0530

    Update Sumofnumbers.java

--- Task/Sumofnumbers.java
+++ Task/Sumofnumbers.java
@@ -1,10 +1,17 @@
 package My.Task;
+import java.util.Scanner;
 
 public class Sumofnumbers {
 
 	public static void main(String[] args) {
-	
-
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter the number:- ");
+	int num = in.nextInt();
+	int val = Number(num);
+	System.out.println("The Sum of the Number is " +val);
 	}
-
+  public static int Number(int num) {
+	  int no = num *((num + 1) / 2);
+	  return no;
+  }
 }
